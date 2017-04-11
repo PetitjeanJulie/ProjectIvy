2fd96618ed455b5dacb747a21cc371dbbcbe3818
Petitjean Julie <jpetitje@SA203-07A.ensi-bourges.fr>
2017-04-11 15:22:19 +0200

    modifié :         build.xml 	nouveau fichier : build/example/Hello.class 	nouveau fichier : data.csv 	nouveau fichier : ivy/ivy.jar 	modifié :         src/example/Hello.java

--- src/example/Hello.java
+++ src/example/Hello.java
@@ -2,6 +2,11 @@
 package example;
 
 import org.apache.commons.lang.WordUtils;
+import com.opencsv.*;
+import java.util.List;
+import java.io.FileReader;
+
+
 
 public class Hello {
     public static void main(String[] args) {
@@ -9,6 +14,16 @@ public class Hello {
         System.out.println("standard message : " + message);
         System.out.println("capitalized by " + WordUtils.class.getName() 
                                 + " : " + WordUtils.capitalizeFully(message));
+
+	int monmax=0;
+	CSVReader reader = new CSVReader(new FileReader("data.csv"));
+	List<String[]>myEntries=reader.readAll();
+	for (String[] ligne = myEntries){
+		for (String[] Caratere = ligne)
+		
+	}
+	System.out.println(monmax);
+	
     }
 }
         
