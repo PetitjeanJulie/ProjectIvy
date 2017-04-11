73475b38811712e8bc32fdecc1870d5147c3be7b
Petitjean Julie <jpetitje@SA203-07A.ensi-bourges.fr>
2017-04-11 15:37:18 +0200

    modifié :         src/example/Hello.java

--- src/example/Hello.java
+++ src/example/Hello.java
@@ -4,7 +4,7 @@ package example;
 import org.apache.commons.lang.WordUtils;
 import com.opencsv.*;
 import java.util.List;
-import java.io.FileReader;
+import java.io.*;
 
 
 
@@ -12,15 +12,18 @@ public class Hello {
     public static void main(String[] args) {
         String  message = "hello ivy !";
         System.out.println("standard message : " + message);
-        System.out.println("capitalized by " + WordUtils.class.getName() 
-                                + " : " + WordUtils.capitalizeFully(message));
+        System.out.println("capitalized by " + WordUtils.class.getName() + " : " + WordUtils.capitalizeFully(message));
 
 	int monmax=0;
 	CSVReader reader = new CSVReader(new FileReader("data.csv"));
 	List<String[]>myEntries=reader.readAll();
-	for (String[] ligne = myEntries){
-		for (String[] Caratere = ligne)
-		
+	for (String ligne : myEntries){
+		for (String Caractere : ligne){
+			int nombre = Integer.parseInt(Caractere);
+			if (nombre > monmax) {
+				monmax = nombre;
+			}
+		}	
 	}
 	System.out.println(monmax);
 	
